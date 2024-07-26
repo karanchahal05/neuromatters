675d82660f30455948ef4c2e20de256c224c0185
Karan Chahal <karanchahal2005.1@gmail.com>
2024-07-26 16:07:46 -0400

    remove database.php

--- database.php
+++
@@ -1,8 +0,0 @@
-<?php
-  $dbServername = "localhost";
-  $dbUsername = "root";
-  $dbPassword = "";
-  $dbName = "Neuromatters";
-
-  $conn = mysqli_connect($dbServername, $dbUsername, $dbPassword, $dbName);
-?>
